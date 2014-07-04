a0750f5aa46af3a02bd59741e752c0702cff40c8
josuemnb <josuemnb@gmail.com>
2014-07-05 00:23:40 +0100

    Create XWindow.h

---
+++ XWindow.h
@@ -0,0 +1,17 @@
+#ifndef XWINDOW_H
+#define XWINDOW_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "XGui.h"
+
+XWidget *XWindowNew(void);
+//void XWindowEvent(XWidget *w, char event);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // XWINDOW_H
