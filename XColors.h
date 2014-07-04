ae5699ec3e3bed40697afdab0f55438158ccdc7c
josuemnb <josuemnb@gmail.com>
2014-07-05 00:19:14 +0100

    Create XColors.h

---
+++ XColors.h
@@ -0,0 +1,26 @@
+#ifndef XCOLORS_H
+#define XCOLORS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define LIGHTGRAY 	0xDCDCDC
+#define VERYLGRAY	0xD3D3D3
+#define GRAY		0xBEBEBE
+#define DARKGRAY 	0x808080
+#define WINDOWCOLOR	0xEEEEEE
+#define BUTTONCOLOR	0xECECEC
+#define WHITE		0xFFFFFF
+#define BLACK		0x000000
+#define BLUE		0x6495ED
+#define LIGHTBLUE	0x87CEFA
+#define VERYLBLUE	0xB0E0E6
+#define CYAN		0x00FFFF
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif // XCOLORS_H
